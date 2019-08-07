95219e8b71acf0de884f140ebc5638d8bd505daa
Jai Ko <just.jai.ko@gmail.com>
2019-08-06 18:26:17 -0700

    factor out and clean up

--- src/functions/tasks.js
+++ src/functions/tasks.js
@@ -1,1 +1,17 @@
-export function lastSubTask() {}
+import uuid from '@/functions/uuid';
+
+export function getEmptyTask(seed) {
+  return {
+    id: uuid(),
+    isProject: false,
+    hasOwnBoard: false,
+    superTaskId: null,
+    subTaskIds: [],
+    prevTaskId: null,
+    nextTaskId: null,
+    description: '',
+    progress: 0,
+    showSubtasks: false,
+    ...seed,
+  };
+}
