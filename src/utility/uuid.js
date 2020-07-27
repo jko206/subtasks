352fedb469e35482d0d5c26386a5cf316ee7a2ba
J Ko <jko@amazon.com>
2020-07-27 08:23:11 -0700

    Make uuid more readable

--- src/utility/uuid.js
+++ src/utility/uuid.js
@@ -1,8 +1,13 @@
-const chars = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`
+const upperCases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowerCases = upperCases.toLowerCase()
+const numerals = '0123456789'
+const chars = `${upperCases}${lowerCases}${numerals}`
 
-export default () => {
+const uuid = () => {
   let str = ''
   let count = 20
   while (count--) str += chars[Math.floor(Math.random() * chars.length)]
   return str
 }
+
+export default uuid
